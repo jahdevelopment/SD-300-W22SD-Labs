9a5e8025c4d64796f767850bf22cbabbbc19cef1
Juan Alberto Hernandez <jualherac@gmail.com>
2022-06-21 10:35:36 -0500

    Initial commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class VendingMachine
+{
+    public int SerialNumber { get; set; }
+
+    Dictionary<int, int> MoneyFloat = new Dictionary<int, int>();
+
+    Dictionary<Product, int> Inventory = new Dictionary<Product, int>();
+
+    public string StockItem(Product product, int quantity)
+    {
+        return 
+    }
+
+    public string StockFloat(int moneyDenomination, int quantity)
+    {
+        return
+    }
+
+     public string VendItem(string code, List<int> money)
+    {
+        return
+    }
+}
+
+class Product
+{
+    public string Name { get; set; }
+    public int Price { get; set; }
+
+    public int Quantity { get; set; }
+
+    public string Code { get; set; }
+
+
+}
+
